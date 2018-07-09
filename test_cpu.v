80fd3397f10cf4be1e1f7dde9f0f21190d826771
Iris Li <461729541@qq.com>
2018-07-09 12:54:40 +0800

    尚未通过仿真

--- test_cpu.v
+++ test_cpu.v
@@ -1,17 +1,40 @@
-
+`timescale 1ns/1ps
 module test_cpu();
 	
 	reg reset;
 	reg clk;
+	reg [7:0] switch; //不知道是用来干什么的
+	reg uart_rx;
+	wire [7:0] led;
+	wire [11:0] digi;
+	wire uart_tx;
 	
-	CPU cpu1(reset, clk);
+	CPU cpu1(reset, clk, led, switch, digi, uart_rx, uart_tx);
 	
 	initial begin
-		reset = 1;
-		clk = 1;
-		#100 reset = 0;
+		reset <= 1;
+		clk <= 1;
+		switch <= 8'b10101010;
+		uart_rx <= 0;
+		#5000 reset <= 0;
 	end
-	
-	always #50 clk = ~clk;
+	initial fork
+		forever begin
+			#5 clk = ~clk;
+		end
+		forever begin
+			#200 uart_rx <= 1'b0;
+			#50 uart_rx <= 1'b0;
+			#50 uart_rx <= 1'b0;
+			#50 uart_rx <= 1'b0;
+			#50 uart_rx <= 1'b1;
+			#50 uart_rx <= 1'b0;
+			#50 uart_rx <= 1'b1;
+			#50 uart_rx <= 1'b1;
+			#50 uart_rx <= 1'b0;
+			#50 uart_rx <= 1'b1;
+			
+		end
+	join
 		
 endmodule
