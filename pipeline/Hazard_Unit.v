4e813f1045f85951582bd36419266de8f9e45792
myEarthOrange <earthleocheng@gmail.com>
2018-07-09 15:26:45 +0800

    修复了跳转指令的bug

--- pipeline/Hazard_Unit.v
+++ pipeline/Hazard_Unit.v
@@ -2,7 +2,7 @@
 load-and-use stall
 jump stall
 beq stall 
-reg-beq stall ???
+reg-beq stall
 */
 
 module Hazard_Unit(
@@ -21,47 +21,53 @@ module Hazard_Unit(
 
   output reg PCWrite,
   output reg IF_ID_RegWrite,
-  output reg ID_EX_Clear; // 1代表清空ID_EX寄存器
+  output reg ID_EX_Clear, // 1代表清空ID_EX寄存器
+  output reg IF_ID_Clear  // 1代表清空IF/ID寄存器
 );
 
-reg cur_jump;
-reg pre_jump;
-reg cur_branch;
-reg pre_branch;
-
 always @(posedge reset or posedge clk) begin
-  cur_jump <= IDcontrol_Jump;
-  pre_jump <= cur_jump;
-  cur_branch <= IDcontrol_Branch;
-  pre_branch <= cur_branch;
 
   if (reset) begin
     PCWrite <= 1;
     IF_ID_RegWrite <= 1;
     ID_EX_Clear <= 0;
+    IF_ID_Clear <= 0;
   end
+
   else begin
     if (ID_EX_MemRd && ((ID_EX_RegRt==IF_ID_RegRs) || (ID_EX_RegRt==IF_ID_RegRt))) begin
       PCWrite <= 0;
       IF_ID_RegWrite <= 0;
       ID_EX_Clear <= 1;
+      IF_ID_Clear <= 0;
     end
-    if (pre_jump || pre_branch) begin
-      PCWrite <= 0;
-      IF_ID_RegWrite <= 0;
-      ID_EX_Clear <= 1;
+    if (IDcontrol_Jump) begin
+      PCWrite <= 1;
+      IF_ID_RegWrite <= 1;
+      ID_EX_Clear <= 0;
+      IF_ID_Clear <= 1;
     end
-    if (cur_branch && ID_EX_RegWrite && (
-        ((IF_ID_RegRs==ID_EX_RegRt) &&  ID_EX_RegDst_0) || 
-        ((IF_ID_RegRs==ID_EX_RegRd) && ~ID_EX_RegDst_0) || 
-        ((IF_ID_RegRt==ID_EX_RegRt) &&  ID_EX_RegDst_0) || 
-        ((IF_ID_RegRt==ID_EX_RegRd) && ~ID_EX_RegDst_0) ) 
-       ) begin
-      PCWrite <= 0;
-      IF_ID_RegWrite <= 0;
-      ID_EX_Clear <= 1;
-    end
+    if (IDcontrol_Branch) begin
+      if (ID_EX_RegWrite && (
+         ((IF_ID_RegRs==ID_EX_RegRt) &&  ID_EX_RegDst_0) || 
+         ((IF_ID_RegRs==ID_EX_RegRd) && ~ID_EX_RegDst_0) || 
+         ((IF_ID_RegRt==ID_EX_RegRt) &&  ID_EX_RegDst_0) || 
+         ((IF_ID_RegRt==ID_EX_RegRd) && ~ID_EX_RegDst_0) ) 
+         ) begin
+           PCWrite <= 0;
+           IF_ID_RegWrite <= 0;
+           ID_EX_Clear <= 1;
+           IF_ID_Clear <= 0;
+         end
+      else begin
+        PCWrite <= 1;
+        IF_ID_RegWrite <= 1;
+        ID_EX_Clear <= 0;
+        IF_ID_Clear <= 1;
+      end
+    end    
   end
+
 end
 
 endmodule
