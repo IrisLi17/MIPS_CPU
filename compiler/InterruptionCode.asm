831512f713b5c34ab672fec5e1e66609f7eb956a
myEarthOrange <earthleocheng@gmail.com>
2018-07-12 10:02:58 +0800

    保护现场

--- compiler/InterruptionCode.asm
+++ compiler/InterruptionCode.asm
@@ -1,6 +1,15 @@
 #interruption processing code
 #假设待显示的两个8bit数放在寄存器$s0和$s1中
 
+addi    $sp, $sp, -28
+sw      $t6, 24($sp)
+sw      $t5, 20($sp)
+sw      $t4, 16($sp)
+sw      $t3, 12($sp)
+sw      $t2, 8($sp)
+sw      $t1, 4($sp)
+sw      $t0, 0($sp)
+
 lui     $t0, 0x4000
 lw      $t1, 8($t0)
 addi    $t2, $zero, 0xFFF9
@@ -9,15 +18,11 @@ sw      $t1, 8($t0)
 
 #首先检查串口是否有数据接收
 lw      $t1, 32($t0) #forwarding
-sll     $zero, $zero, 0 #空指令
 andi    $t2, $t1, 0x0008 #forwarding
 beq     $t2, $zero, noload #forwarding
-sll     $zero, $zero, 0 #空指令
 beq     $s0, $zero, loads0 #如果$s0==0，那么向$s0加载数据，否则向$s1加载数据
-sll     $zero, $zero, 0 #空指令
 lw      $s1, 28($t0)
 j		noload
-sll     $zero, $zero, 0 #空指令
 loads0:
 lw      $s0, 28($t0)
 
@@ -25,32 +30,25 @@ lw      $s0, 28($t0)
 #其次实现数码管的扫描显示
 noload:
 lw		$t1, 20($t0)
-
 srl     $t4, $s1, 4	#本来应位于line 12，插入此处避免stall
 andi    $t2, $t1, 0x0100 #forwarding
 beq		$t2, $zero, target1	#if... #forwarding
-sll     $zero, $zero, 0 #空指令
 addi    $t3, $zero, 0x0200 #即将点亮AN1
-j		finish	
-sll     $zero, $zero, 0 #空指令			
+j		finish			
 
 target1:
 andi    $t2, $t1, 0x0200 #forwarding
 beq     $t2, $zero, target2 #elseif... #forwarding
-sll     $zero, $zero, 0 #空指令
 addi    $t3, $zero, 0x0400 #即将点亮AN2
 andi    $t4, $s0, 0x000F
 j       finish	
-sll     $zero, $zero, 0 #空指令
 
 target2:
 andi    $t2, $t1, 0x0400 #forwarding
 beq     $t2, $t1, target3 #elseif... #forwarding
-sll     $zero, $zero, 0 #空指令
 addi    $t3, $zero, 0x0800 #即将点亮AN3
 srl     $t4, $s0, 4
 j		finish			
-sll     $zero, $zero, 0 #空指令	
 
 target3: #else...
 addi    $t3, $zero, 0x0100 #即将点亮AN0
@@ -58,12 +56,20 @@ andi    $t4, $s1, 0x000F
 
 finish:
 lw      $t5, 0($t4) #forwarding
-sll     $zero, $zero, 0 #这条指令是nop
 add     $t6, $t5, $t3 #forwarding
 sw      $t6, 20($t0) #forwarding
 
 lw      $t1, 8($t0)
-ori     $t1, $t1, 0x0002
-sw      $t1, 8($t0)
+ori     $t2, $t1, 0x0002
+sw      $t2, 8($t0)
+
+lw      $t0, 0($sp)
+lw      $t1, 4($t1)
+lw      $t2, 8($sp)
+lw      $t3, 12($sp)
+lw      $t4, 16($sp)
+lw      $t5, 20($sp)
+lw      $t6, 24($sp)
+addi    $sp, $sp, 28
 
 jr		$k0					
