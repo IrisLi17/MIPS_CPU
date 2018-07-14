cabecc50efa32a9ca9767904c0f21bc85348c531
Iris Li <461729541@qq.com>
2018-07-14 09:15:16 +0800

    增加s5,s6寄存器用于重复接收操作数

--- compiler/gcd_main.asm
+++ compiler/gcd_main.asm
@@ -1,7 +1,7 @@
 lui     $t5, 0x4000
 sw      $zero, 8($t5)
 lui     $t4, 0xffff
-addi    $t4, $zero, 0xe000
+addi    $t4, $zero, 0xff00
 sw      $t4, 0($t5)
 nor     $t6, $zero, $zero
 sw      $t6, 4($t5)
@@ -9,16 +9,16 @@ addi    $t4, $zero, 0x0003
 sw      $t4, 8($t5)
 
 loop:
-slt     $t0, $zero, $s0
-slt     $t1, $zero, $s1 
+slt     $t0, $zero, $s5
+slt     $t1, $zero, $s6 
 and     $t2, $t0, $t1 # forwarding
 bne		$t2, $zero, target # forwarding
-add		$s2, $s0, $zero # 无关指令，防止阻塞
+add		$s2, $s5, $zero # 无关指令，防止阻塞
 j		loop
 sll     $zero, $zero, 0
 
 target:
-add     $s3, $s1, $zero
+add     $s3, $s6, $zero
 compare:
 slt     $t3, $s2, $s3 # forwarding
 beq     $t3, $zero, s2greater # forwarding
@@ -39,6 +39,8 @@ finish: # $s3 is gcd
 lui     $t5,0x4000
 sw		$s3, 24($t5) # uart_txd
 sw      $s3, 12($t5) # led
+add     $s5, $zero, $zero
+add     $s6, $zero, $zero # $s0,$s1清零以便判断需要load新数据
 j		Exit		 # 手动加机器码
 Exit:
 lui     $t0,0x4000
