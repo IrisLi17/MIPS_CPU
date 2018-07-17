69ef1ccf434f685aa884f65c46db1f061c04a805
Iris Li <461729541@qq.com>
2018-07-17 20:48:29 +0800

    50MHz单周期

--- singleCycle/cpu_clk.v
+++ singleCycle/cpu_clk.v
@@ -1,23 +1,12 @@
 module cpu_clk(sys_clk,clk);
 input sys_clk;
 output reg clk;
-reg [2:0] counter;
-
 initial begin
-  counter<=3'd0;
   clk<=0;
 end
 
 always @(posedge sys_clk) begin
-  case (counter)
-    3'b111: begin
-      counter<=3'd0;
-      clk<=~clk;
-    end
-    default: begin
-      counter <= counter + 3'b001;
-    end
-  endcase
+  clk <= ~clk;
 end
 
 endmodule
